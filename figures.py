1605741814e0a572bd0a8a963d975c9aa8cc1189
casualdoto <khrestyanovskii@gmail.com>
2024-11-11 17:31:53 +0300

    added classes ShapeCorrected, RectangleCorrected

--- figures.py
+++ figures.py
@@ -46,7 +46,52 @@ class Square(Rectangle):
         return (f"{Shape.__repr__(self)}, со стороной {self.width},"
                 f" с площадью {self.area()} и периметром {self.perimeter()}")
 
+class ShapeCorrected:
+    """Геометрические фигуры"""
+    name = 'геометрическая фигура'
 
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
+
+    def __repr__(self):
+        return f"{self.name} по координатам ({self.__x}, {self.__y})"
+
+
+class RectangleCorrected(ShapeCorrected):
+    """Прямоугольники"""
+    name = 'прямоугольник'
+
+    def __init__(self, width, height, x=0, y=0):
+        super().__init__(x, y)
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __repr__(self):
+        return (f"{ShapeCorrected.__repr__(self)}, со сторонами {self.width} и {self.height},"
+                f" с площадью {self.area()} и периметром {self.perimeter()}")
 
 
 
