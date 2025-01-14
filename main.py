5d0671c58656bd90b1772fe8048cb6f3b3733d01
shmel9va <168944627+shmel9va@users.noreply.github.com>
2025-01-14 06:16:35 +0300

    edit files structure

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from figures import Rectangle, Square
+from Rectangle import Rectangle
+from Square import Square
+from RectangleCorrected import RectangleCorrected
+from SquareCorrected import SquareCorrected
 
-print('Изменим ширину и высоту фигур, описанных в коде, нарушающем принцип подстановки Барбары Лисков\n')
-# Создаем список фигур
-figures = [Rectangle(2, 3), Square(2, 1, 1)]
+# Изменим ширину и высоту фигур, описанных в коде, нарушающем принцип подстановки Барбары Лисков
+figures = [Rectangle(2, 3), Square(2, 1, 1)] # Создаем список фигур
 
 # Изменяем ширину и высоту каждой фигуры
 for figure in figures:
@@ -11,17 +13,18 @@ for figure in figures:
     figure.set_height(10)
     print(f"После изменения:{figure}\n")
 
-print('Для объекта Square это привело к тому, что стороны квадрата перестали оставаться равными.')
+# Для объекта Square это привело к тому, что стороны квадрата перестали оставаться равными
 
-from figures import RectangleCorrected, SquareCorrected
 
-print("\nТестируем исправленную реализацию (LSP):")
-figures = [RectangleCorrected(2, 3), SquareCorrected(2, 1, 1)]
 
+# Тестируем исправленную реализацию (LSP):
+figures = [RectangleCorrected(2, 3), SquareCorrected(2, 1, 1)] # Создаем список фигур
+
+# Изменяем ширину и высоту каждой фигуры
 for figure in figures:
     print("До изменения:", figure)
     figure.width = 5
     figure.height = 10
     print(f"После изменения: {figure}\n")
 
-print('Для объекта SquareCorrected стороны квадрата всегда остаются равными после изменений.\n')
+# Для объекта SquareCorrected стороны квадрата всегда остаются равными после изменений
